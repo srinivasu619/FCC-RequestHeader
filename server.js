957759ec96dccaead57c3b0ee15bf163995e4d3a
srinivasu619 <srinivasu.618@gmail.com>
2017-07-27 23:30:03 +0530

    Added main logic

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 var express = require('express');
+var platform = require('platform');
 var app = express();
+app.enable('trust proxy');
+app.set('view engine','ejs');
 app.get('/',(req,res)=>{
   res.sendFile( __dirname + "/views/index.html" );
+})
+app.get('/whoami',function(req,res){
+	var header = req.headers;
+	var ipAdd = req.ip;
+	var UA = platform.parse(header["user-agent"]);
+	var language = header["accept-language"].split(';')[0];
+	var info ={ip:ipAdd,OSInfo:UA.os,language:language};
+	res.render('main',{info:info});
 })
 
 var listener = app.listen(process.env.PORT, function () {
